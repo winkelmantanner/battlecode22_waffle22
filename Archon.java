b0ede1fb5f5a6f938254f971aec02b35215c8804
Tanner Winkelman <winkelmantanner@gmail.com>
2022-01-08 20:30:18 -0600

    Fix the bug in which only one Archon builds

--- Archon.java
+++ Archon.java
@@ -9,13 +9,18 @@ public class Archon extends Building {
     
     RobotType typeToBuild = RobotType.MINER /* miner first */;
     
+    int numRobotsBuilt = 0;
+    
     @Override
     public void runTypeSpecific() throws GameActionException {
         
         for(int k = 0; k < 10; k++) {
             final Direction randomDir = Utils.choice(rng, Direction.allDirections());
-            if(rc.canBuildRobot(typeToBuild, randomDir)) {
+            if(rc.getTeamLeadAmount(rc.getTeam()) > typeToBuild.buildCostLead + (5 * numRobotsBuilt)
+                && rc.canBuildRobot(typeToBuild, randomDir)
+            ) {
                 rc.buildRobot(typeToBuild, randomDir);
+                numRobotsBuilt++;
                 typeToBuild = Utils.choice(rng, new RobotType[] {RobotType.MINER, RobotType.SOLDIER});
                 break;
             }
