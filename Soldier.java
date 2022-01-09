5bfc457c825ff4b69238db389af79ef3b25eb7a6
Tanner Winkelman <winkelmantanner@gmail.com>
2022-01-09 16:43:04 -0600

    Make soldiers communicate to go toward the most successful soldier

--- Soldier.java
+++ Soldier.java
@@ -7,25 +7,12 @@ public class Soldier extends Droid {
         super(rc);
     }
     
+    int totalDamageDone = 0;
+    
+    final int BEST_SOLDIER_EXPIRATION_ROUNDS = 50;
+    
     @Override
     public void runTypeSpecific() throws GameActionException {
-        RobotInfo leader = null;
-        for(RobotInfo friendlyRbt : rc.senseNearbyRobots(
-            rc.getType().visionRadiusSquared,
-            rc.getTeam()
-        )) {
-            if(friendlyRbt.type.equals(RobotType.SOLDIER)
-                && (leader == null
-                    || friendlyRbt.ID < leader.ID
-                )
-            ) {
-                leader = friendlyRbt;
-            }
-        }
-        if(leader != null) {
-            stepAvoidingRubble(leader.getLocation());
-        }
-        
         for(RobotInfo enemyRbt : rc.senseNearbyRobots(
             rc.getType().visionRadiusSquared,
             rc.getTeam().opponent()
@@ -33,10 +20,34 @@ public class Soldier extends Droid {
             stepAvoidingRubble(enemyRbt.getLocation());
             if (rc.canAttack(enemyRbt.getLocation())) {
                 rc.attack(enemyRbt.getLocation());
+                totalDamageDone += rc.getType().damage;
+                if(totalDamageDone > 0
+                    && (
+                        totalDamageDone > rc.readSharedArray(BEST_SOLDIER_DAMAGE_DONE_IDX)
+                        || rc.getRoundNum() - rc.readSharedArray(BEST_SOLDIER_ROUND_IDX) > BEST_SOLDIER_EXPIRATION_ROUNDS
+                    )
+                ) {
+                    rc.writeSharedArray(BEST_SOLDIER_DAMAGE_DONE_IDX, totalDamageDone);
+                    rc.writeSharedArray(BEST_SOLDIER_LOC_X_IDX, rc.getLocation().x);
+                    rc.writeSharedArray(BEST_SOLDIER_LOC_Y_IDX, rc.getLocation().y);
+                    rc.writeSharedArray(BEST_SOLDIER_ROUND_IDX, rc.getRoundNum());
+                }
                 break;
             }
         }
-        
+
+        if(rc.readSharedArray(BEST_SOLDIER_DAMAGE_DONE_IDX) > totalDamageDone
+            && rc.getRoundNum() - rc.readSharedArray(BEST_SOLDIER_ROUND_IDX) < BEST_SOLDIER_EXPIRATION_ROUNDS
+        ) {
+            stepAvoidingRubble(new MapLocation(
+                rc.readSharedArray(BEST_SOLDIER_LOC_X_IDX),
+                rc.readSharedArray(BEST_SOLDIER_LOC_Y_IDX)
+            ));
+//            rc.setIndicatorString("GOING TOWARD " + rc.readSharedArray(BEST_SOLDIER_LOC_X_IDX) + ", " + rc.readSharedArray(BEST_SOLDIER_LOC_Y_IDX)
+//                + " WHERE SOLDIER WHO HAD DONE " + rc.readSharedArray(BEST_SOLDIER_DAMAGE_DONE_IDX)
+//                + " WAS " + (rc.getRoundNum() - rc.readSharedArray(BEST_SOLDIER_ROUND_IDX)) + " ROUNDS AGO"
+//            );
+        }
         simpleExploreMove();
     }
 }
