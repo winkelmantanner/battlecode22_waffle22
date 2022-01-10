4a9e216ef73d88ff621852e2c814aaf5996dac92
Tanner Winkelman <21373626+winkelmantanner@users.noreply.github.com>
2022-01-09 19:04:59 -0600

    Make soldiers prioritize enemies that have already been damaged

--- Soldier.java
+++ Soldier.java
@@ -8,32 +8,46 @@ public class Soldier extends Droid {
     }
     
     int totalDamageDone = 0;
+    MapLocation lastTargetRbtLoc = null;
+    int lastTargetRbtRound = -12345;
     
     final int BEST_SOLDIER_EXPIRATION_ROUNDS = 50;
     
     @Override
     public void runTypeSpecific() throws GameActionException {
+        RobotInfo targetRbt = null;
+        double bestTargetValue = -12345;
         for(RobotInfo enemyRbt : rc.senseNearbyRobots(
             rc.getType().visionRadiusSquared,
             rc.getTeam().opponent()
         )) {
-            stepAvoidingRubble(enemyRbt.getLocation());
-            if (rc.canAttack(enemyRbt.getLocation())) {
-                rc.attack(enemyRbt.getLocation());
-                totalDamageDone += rc.getType().damage;
-                if(totalDamageDone > 0
-                    && (
-                        totalDamageDone > rc.readSharedArray(BEST_SOLDIER_DAMAGE_DONE_IDX)
-                        || rc.getRoundNum() - rc.readSharedArray(BEST_SOLDIER_ROUND_IDX) > BEST_SOLDIER_EXPIRATION_ROUNDS
-                    )
-                ) {
-                    rc.writeSharedArray(BEST_SOLDIER_DAMAGE_DONE_IDX, totalDamageDone);
-                    rc.writeSharedArray(BEST_SOLDIER_LOC_X_IDX, rc.getLocation().x);
-                    rc.writeSharedArray(BEST_SOLDIER_LOC_Y_IDX, rc.getLocation().y);
-                    rc.writeSharedArray(BEST_SOLDIER_ROUND_IDX, rc.getRoundNum());
+            if(rc.canAttack(enemyRbt.location)) {
+                final double value = (double)enemyRbt.getType().health / enemyRbt.getHealth();
+                if(value > bestTargetValue) {
+                    targetRbt = enemyRbt;
+                    bestTargetValue = value;
                 }
-                break;
             }
+        }
+        if (targetRbt != null) {
+            lastTargetRbtLoc = targetRbt.location;
+            lastTargetRbtRound = rc.getRoundNum();
+            rc.attack(targetRbt.getLocation());
+            totalDamageDone += rc.getType().damage;
+            if(totalDamageDone > 0
+                && (
+                    totalDamageDone > rc.readSharedArray(BEST_SOLDIER_DAMAGE_DONE_IDX)
+                    || rc.getRoundNum() - rc.readSharedArray(BEST_SOLDIER_ROUND_IDX) > BEST_SOLDIER_EXPIRATION_ROUNDS
+                )
+            ) {
+                rc.writeSharedArray(BEST_SOLDIER_DAMAGE_DONE_IDX, totalDamageDone);
+                rc.writeSharedArray(BEST_SOLDIER_LOC_X_IDX, rc.getLocation().x);
+                rc.writeSharedArray(BEST_SOLDIER_LOC_Y_IDX, rc.getLocation().y);
+                rc.writeSharedArray(BEST_SOLDIER_ROUND_IDX, rc.getRoundNum());
+            }
+        }
+        if(rc.getRoundNum() - lastTargetRbtRound < 10) {
+            stepAvoidingRubble(lastTargetRbtLoc);
         }
 
         if(rc.readSharedArray(BEST_SOLDIER_DAMAGE_DONE_IDX) > totalDamageDone
